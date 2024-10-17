ca2bc78788396ae6c718d3d627ab3cf37af69205
fullStackJourney <mdratul47782@example.com>
2024-10-17 14:20:37 +0600

    Describe the changes you made

--- src/Components/main/Income.jsx
+++ src/Components/main/Income.jsx
@@ -124,26 +124,28 @@ export default function Income({ tasks, deleteIncome,onEdit }) {
               onClick={() => setFilteringOpen(!isFilteringOpen)}
             >
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="18"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="icon-tabler icon-tabler-adjustments-alt"
-              >
-                <path d="M4 8h4v4h-4z" />
-                <path d="M6 4l0 4" />
-                <path d="M6 12l0 8" />
-                <path d="M10 14h4v4h-4z" />
-                <path d="M12 4l0 10" />
-                <path d="M12 18l0 2" />
-                <path d="M16 5h4v4h-4z" />
-                <path d="M18 4l0 1" />
-                <path d="M18 9l0 11" />
-              </svg>
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="18"
+                          height="18"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          class="icon icon-tabler icons-tabler-outline icon-tabler-adjustments-alt"
+                        >
+                          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                          <path d="M4 8h4v4h-4z" />
+                          <path d="M6 4l0 4" />
+                          <path d="M6 12l0 8" />
+                          <path d="M10 14h4v4h-4z" />
+                          <path d="M12 4l0 10" />
+                          <path d="M12 18l0 2" />
+                          <path d="M16 5h4v4h-4z" />
+                          <path d="M18 4l0 1" />
+                          <path d="M18 9l0 11" />
+                        </svg>
             </button>
 
             {isFilteringOpen && (
